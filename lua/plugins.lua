7ab55b02a097590d66a81c745da5e0de981c0107
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-01-26 21:43:35 +0100

    setup telescope ui select

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,5 +11,26 @@ return {
         'folke/which-key.nvim',
         opts = {}
     },
-    { "nvim-telescope/telescope-ui-select.nvim" }
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    },
+    {
+        'goolord/alpha-nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.startify'.config)
+        end
+    }
 }
