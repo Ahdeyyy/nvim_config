ad908e4151470050e9e1dc9c682ff86cc9dddcbc
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-01-26 19:00:26 +0100

    changed theme to poimandres

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,17 @@
 return {
-    "dracula/vim",
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'dracula'
+        require('poimandres').setup {
+            -- leave this setup function empty for default config
+            -- or refer to the configuration section
+            -- for configuration options
+        }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+        vim.cmd("colorscheme poimandres")
     end
 }
