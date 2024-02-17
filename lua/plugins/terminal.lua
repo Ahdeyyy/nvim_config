037829c139e5131fc282c8fe83cadef1acf83fd7
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-02-17 18:01:19 +0100

    made the border float

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -9,9 +9,13 @@ return {
             shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
             shading_factor = '10',
             shell = 'nu',
-            border = 'curved',
             persist_size = true,
             persist_mode = true,
+            direction = 'float',
+            auto_scroll = true,
+            float_opts = {
+                border = 'curved'
+            },
         }
     }
 }
