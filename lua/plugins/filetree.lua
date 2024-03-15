a0d28a9df6a0d751e827b96a04443c4aab19437d
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-03-15 13:02:51 +0100

    made hidden files visible in neo tree

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -3,12 +3,22 @@ return {
     keys = { {
         "<leader>ft",
         "<cmd>Neotree toggle<cr>",
-        desc = "NeoTree"
+        desc = "Toggle NeoTree",
     } },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
         "3rd/image.nvim",
-    }
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = false, -- when true, they will just be displayed differently than normal items
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_hidden = false, -- only works on Windows for hidden files/directories
+            },
+        },
+    },
 }
