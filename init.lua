f2e1f72daba64d47451971d2eacff590b2c4c6b0
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-09-11 14:13:39 +0100

    added lazy git toggle

--- init.lua
+++ init.lua
@@ -5,14 +5,14 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,14 +21,14 @@ require("lazy").setup("plugins")
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", {
-	clear = true,
+  clear = true,
 })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
 })
 -- neovide configs
 require("neovide")
@@ -39,3 +39,16 @@ require("vim-opts")
 --
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", hidden = true })
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>gl",
+  "<cmd>lua _lazygit_toggle()<CR>",
+  { desc = "open lazygit", noremap = true, silent = true }
+)
